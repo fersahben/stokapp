0396280190228b3e486be8a50211299fcfcf7137
fersahben <fersahben@gmail.com>
2025-01-26 15:04:11 +0300

    Update build process to handle numpy installation separately

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# pip'i güncelle
+pip install --upgrade pip
+
+# Önce numpy'ı kur
+pip install numpy==1.20.3 --only-binary :all:
+
+# Diğer gereksinimleri kur
 pip install -r requirements.txt
 
 # Gerekli dizinleri oluştur
